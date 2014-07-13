d9a2062e2d35405929cd4a38e54fdbaf71b49ca8
Bretto9 <jose.bretto@gmail.com>
2014-07-13 19:50:24 +0200

    Proyecto Limpio

---
+++ LadyFoo/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: JoseDavid
+ *
+ * Created on 13 de julio de 2014, 19:48
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
